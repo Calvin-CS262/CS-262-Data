9930112e430f88b7e04ab4de819ab6077e814ced
Luke Steffen <lhs3@gold12.cs.calvin.edu>
2018-10-23 20:49:59 -0400

    script to create database

---
+++ caluberDatabase.sql
@@ -0,0 +1,40 @@
+-- This script builds a basic database for the CalUber app
+-- 
+-- @author Luke Steffen (lhs3)
+-- Created on 10/23/2018
+
+-- Drop tables if they exist
+DROP TABLE IF EXISTS Passenger;
+DROP TABLE IF EXISTS Ride;
+DROP TABLE IF EXISTS Users;
+
+CREATE TABLE Users (
+	id integer PRIMARY KEY,
+	studentId varchar(5),
+	password varchar(50),
+	lName text,
+	fName text
+	);
+
+CREATE TABLE Ride (
+	id integer PRIMARY KEY,
+	driver integer REFERENCES Users(id),
+	passengerLimit integer,
+	departure text,
+	destination text,
+	day date,
+	time time,
+	status bit
+	);
+
+CREATE TABLE Passenger (
+	rideId integer REFERENCES Ride(id),
+	passengerId integer REFERENCES Users(id),
+	passengerName text
+	);
+
+GRANT SELECT ON Users TO PUBLIC;
+GRANT SELECT ON Ride TO PUBLIC;
+GRANT SELECT ON Passenger TO PUBLIC;
+
+
